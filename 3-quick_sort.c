b1c142840b03ea9717a583bc1be32fe7409425a4
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-02-20 18:15:34 +0200

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -73,5 +73,7 @@ void quick_sort_helper(int *array, int low, int high, size_t size)
 
 void quick_sort(int *array, size_t size)
 {
+	if (array == NULL || size < 0)
+		return;
 	quick_sort_helper(array, 0, size - 1, size);
 }
