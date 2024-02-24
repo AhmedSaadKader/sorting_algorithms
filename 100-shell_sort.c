28606e9670cfcabf86c77fd2340d7e89ae3f8ffc
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-02-24 13:54:17 +0200

    100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,6 +1,26 @@
 #include "sort.h"
 
 /**
+ * swap - to swap 2 values
+ * @array: the array
+ * @i: 1st index
+ * @j: 2nd index
+ */
+
+void swap(int *array, int i, int j)
+{
+	int tmp;
+
+	if (array[i] != array[j])
+	{
+		tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+	}
+}
+
+
+/**
  * shell_sort - sorts an array of integers in ascending order using
  * the shell sort algorithm, using the Knuth sequence
  * @array: the array of integers
@@ -9,13 +29,22 @@
 
 void shell_sort(int *array, size_t size)
 {
-	int gap, i, temp, j;
+	size_t i, j, gap = 0;
 
-	for (gap = size / 2; gap > 0; gap /= 2)
+	if (size < 2)
+		return;
+
+	while (gap <= size / 3)
+		gap = gap * 3 + 1;
+
+	while (gap >= 1)
 	{
-		temp = array[i];
-		for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
-			array[j] = array[j - gap];
-		array[j] = temp;
+		for (i = gap; i < size; i++)
+		{
+			for (j = i; j >= gap && array[j] < array[j - gap]; j -= gap)
+				swap(array, j, j - gap);
+		}
+		gap /= 3;
+		print_array(array, size);
 	}
 }
